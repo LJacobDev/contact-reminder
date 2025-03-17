68e09afde19e252133c9cf7fd95a6a9f2f184971
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-03-17 19:51:06 -0400

    Modify InteractionView appearance

--- src/views/InteractionView.vue
+++ src/views/InteractionView.vue
@@ -2,15 +2,27 @@
 
 <template>
   <main>
-    <h1>Interaction View</h1>
-    Interaction Date and Time:<input type="datetime-local" /><br />
-    Interaction Summary: <input /><br />
-    Interaction Details: <textarea name="" id=""></textarea><br />
-    Followup Date and Time:<input type="datetime-local" /><br />
-    <br />
-    Followup Summary: <input /><br />
-    <br />
-    Followup Details: <textarea name="" id=""></textarea>
+    <div style="display: flex; justify-content: space-between">
+      <h1>Interaction Details</h1>
+      <div>
+        <!-- TODO: turn these into better looking button components, these are placeholders -->
+        <button>Save</button>
+        <button>Cancel</button>
+        <button>Delete</button>
+      </div>
+    </div>
+    <h3>Interaction Date and Time:</h3>
+    <input type="datetime-local" />
+    <h3>Interaction Summary:</h3>
+    <input />
+    <h3>Interaction Details:</h3>
+    <textarea name="" id=""></textarea>
+    <h3>Followup Date and Time:</h3>
+    <input type="datetime-local" />
+    <h3>Followup Summary:</h3>
+    <input />
+    <h3>Followup Details:</h3>
+    <textarea name="" id=""></textarea>
   </main>
 </template>
 
